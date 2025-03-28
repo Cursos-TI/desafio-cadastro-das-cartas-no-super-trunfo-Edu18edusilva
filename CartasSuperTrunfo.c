51bdc6e73153ffe65fde07f87c0ec46cae6b2d9a
Edu18edusilva <edu18edusilva@gmail.com>
2025-03-28 10:27:55 +0000

    28/03

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,19 +8,48 @@
 
 int main() {
     
+
     // Declarando as variáveis.
-    char estado;
-    char nomeDaCidade, codigoDaCarta [50];
+
+    char estado [1];
+    char codigoDaCarta [50];
+    char nomeDaCidade [50];
     int populacao, numeroDePontosTuristicos;
     float area, pib;
 
 
     // Dados da primeira cidade.
+
     printf ("Digite a letra do estado: \n");
     scanf ("%s", & estado);
 
     printf ("Digite o código da cidade: \n");
     scanf ("%s", & codigoDaCarta);
 
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s", & nomeDaCidade);
+
+    printf ("Digite a população da cidade: \n");
+    scanf ("%d", & populacao);
+
+    printf ("Digite a área da cidade: \n");
+    scanf ("%f", & area);
+
+    printf ("Digite o PIB da cidade: \n");
+    scanf ("%f", & pib);
+
+    printf ("Digite o número de pontos turísticos da cidade: \n");
+    scanf ("%d", & numeroDePontosTuristicos);
+
+    // Primeira carta
+
+    printf ("Carta 1: \n");
+    printf ("Estado: %s", estado);
+    printf ("Código: %s%s \n", estado, codigoDaCarta);
+    printf ("Nome da cidade: %s \n", nomeDaCidade);
+    printf ("População: %d \n", populacao);
+    printf ("Área: %f \n", area);
+    printf ("PIB: %f \n", pib);
+    printf ("Número de pontos turísticos: %d \n", numeroDePontosTuristicos);
 
 };
